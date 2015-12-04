dbbfa0dc0e3da47c56d014eb9bd605a7ecc6e007
Karl Hepworth <Karl.Hepworth@ssc.gov.au>
2015-12-04 18:52:51 +1100

    adjust much of the rest...

--- wcag.sh
+++ wcag.sh
@@ -32,10 +32,11 @@ function GENERATE_REPORT () {
 	# clear file
 	echo "" > ./checkthesefiles.js
 	for x in "${pages[@]}"; do
-		resultarray+=("\"${URL}/node/${x}\",");
+		resultarray+=("\"http://${URL}/node/${x}\",");
 	done
+	resultarray+="";
 	sed "s|$OLDpageholder|${resultarray[*]}|g" TemplatesTest/access.js >> ./checkthesefiles.js;
-	# node checkthesefiles.js;
+	node checkthesefiles.js; # needs work
 	for X in $(find ./reports/ | grep json); do
 		nodenum=${X%.*};
 		sed -i "s|\[|\"node_$nodenum\": \[|" ${X};
@@ -51,13 +52,13 @@ function GENERATE_REPORT () {
 	done
 	sed -i -e "s|.$|}|" accessibility_report.json;
 	sed -i -e "1s|^|\{\n|" accessibility_report.json;
-	# js-beautify accessibility_report.json;
+	js-beautify accessibility_report.json > accessibility_report.json;
 }
 
 function CLEAN () {
 	# REMOVE FILES
-	# rm -f reports/*
-	# rm -f drupalpages
+	rm -f reports/*
+	rm -f drupalpages
 	rm checkthesefiles.js
 }
 
