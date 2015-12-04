1a9e612dd0d4da4b50ba6a8ad273fed0675df594
Karl Hepworth <Karl.Hepworth@ssc.gov.au>
2015-12-04 17:59:24 +1100

    make the script work for any site - still needs work

--- wcag.sh
+++ wcag.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 
 function GETVAR () {
-	# echo "What is the name of the site you want to check?"
-	# read NAME
-	NAME="ssphelp";
-	# echo "Which environment of the site you want to check?"
-	# read ENV
-	ENV="prod";
-	echo "You have selected $NAME"
-	BASEPATH="/acquia";
-	# BASEPATH="/acquia/20150831123758.loc.gbuild.net";
+	if [[ -n "${1}" ]]; then
+		NAME="${1}";
+	else
+		NAME="";
+	fi
+	if [[ ! "${NAME}" == "" ]]; then
+		URL=$(drush "${NAME}" "status" | grep "Site URI" | cut -d: -f2);
+	else
+		URL=$(drush "status" | grep "Site URI" | cut -d: -f2);
+	fi
+	URL=$(drush "${NAME}" "status" | grep "Site URI" | cut -d: -f2);
+	URL=${URL/  /};
+	URL=${URL/ /};
+	BASEPATH="${PWD}";
 }
 
 function GENERATE_REPORT () {
-	touch "/${BASEPATH}/tests/accessibility/drupalpages";
-	drush "@ssc.ace.${NAME}.${ENV}" sqlq "SELECT nid FROM node  WHERE status=1" > "/${BASEPATH}/tests/accessibility/drupalpages";
+	touch "drupalpages";
+	if [[ ! "${NAME}" == "" ]]; then
+		drush "${NAME}" "sqlq" "SELECT nid FROM node  WHERE status=1" > "/${BASEPATH}/drupalpages";
+	else
+		drush "sqlq" "SELECT nid FROM node  WHERE status=1" > "/${BASEPATH}/drupalpages";
+	fi
 	# # Grab node ids and update access,js
-	cd "${BASEPATH}/tests/accessibility";
 	declare -a pages=($(cat drupalpages));
 	OLDpageholder="pagestested";
 	resultarray=();
 	# clear file
 	echo "" > ./checkthesefiles.js
 	for x in "${pages[@]}"; do
-		resultarray+=("\"http://ssc:sdh93d2c7ae@${ENV}-${NAME}.ace.gbuild.net/node/${x}\",");
+		resultarray+=("\"${URL}/node/${x}\",");
 	done
 	sed "s|$OLDpageholder|${resultarray[*]}|g" TemplatesTest/access.js >> ./checkthesefiles.js;
 	node checkthesefiles.js;
@@ -40,7 +48,7 @@ function GENERATE_REPORT () {
 			cat "${X}" >> accessibility_report.json;
 		fi
 	done
-	sed -i -e "$ s/.$/}/" accessibility_report.json;
+	sed -i -e "s|.$|}|" accessibility_report.json;
 	sed -i '1s|^|\{\n|' accessibility_report.json;
 	js-beautify accessibility_report.json;
 }
@@ -52,6 +60,12 @@ function GENERATE_REPORT () {
 # 	# rm checkthesefiles.js
 # }
 
-GETVAR;
+if [[ -n "${1}" ]] && [[ -n "${2}" ]]; then
+	GETVAR "${1}" "${2}";
+elif [[ -n "${1}" ]]; then
+	GETVAR "${1}";
+elif [[ -z "${1}" ]]; then
+	GETVAR;
+fi
 GENERATE_REPORT;
 # CLEAN;
