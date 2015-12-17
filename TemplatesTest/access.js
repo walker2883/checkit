4dde6863f5364284b3e4f9f17c984afc0ed5f004
Karl Hepworth <Karl.Hepworth@ssc.gov.au>
2015-12-17 15:21:24 +1100

    remove console.log; fix indentation of object

--- TemplatesTest/access.js
+++ TemplatesTest/access.js
@@ -2,16 +2,14 @@ var accessSniff  = require('access-sniff');
 var files = [];
 
 accessSniff.start(files, {
-    accessibilityLevel: 'WCAG2AAA',
-    reportType: 'json',
-    reportLocation : 'reports',
-    domElement: true,
-    verbose:false,
-    reportLevels: {
-      notice: false,
-      warning: false,
-      error: true
-    }
-  });
-
-  console.log('Your report can be found in the reports folder');
+  accessibilityLevel: 'WCAG2AAA',
+  reportType: 'json',
+  reportLocation : 'reports',
+  domElement: true,
+  verbose:false,
+  reportLevels: {
+    notice: false,
+    warning: false,
+    error: true
+  }
+});
